119de66ce34c1dc2691f1f70877ed0f62d146226
Ian Quick <ian.quick@gmail.com>
2015-04-23 16:49:10 -0400

    things

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+ln -sf ~/.spacemacs ./spacemacs
+if [ ! -d ~/.emacs.d ] ; then 
+	git clone --recursive http://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
+rm -rf ~/.emacs.d/private
+ln -sf $PWD/spacemacs/private/ ~/.emacs.d
+if [ ! -d ~/.oh-my-zsh ] ; then
+	curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+fi
+ln -sf $PWD/.gitconfig ~/.gitconfig
